ed476b1f13e0e8e15d57fd341b88bd907557a100
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-09-12 16:40:36 +0900

    📝 variabel.php

---
+++ variabel.php
@@ -0,0 +1,16 @@
+<?php
+    // contoh penulisan variables dalam php
+    // String
+    $title = "SaCode";
+    $tag_line = "Papua Coding School";
+    $tahun = "2023";
+
+    // Menampilkan data dari dalam variabel
+    echo $title;
+    echo "<br>";
+
+    echo $tag_line;
+    echo "<br>";
+    
+    echo $tahun;
+?>
