d65368a4ddd89b3c174988dd94888d7b3237234c
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-09-12 16:28:20 +0900

    📝Add php-html.php

---
+++ php-html.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kombinasi - PHP & HTML</title>
+</head>
+<body>
+
+    <!-- Kombinasi antar HTML & PHP -->
+    <h1>Kombinasi PHP & HTML</h1>
+
+    <?php 
+        echo "Hello PHP!";
+    ?>
+    
+</body>
+</html>
