c1a94e5399977dfa9127f975a7443e59fd58c3f9
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-09-12 16:45:33 +0900

    📝 add echo dan print

---
+++ echo-print.php
@@ -0,0 +1,9 @@
+<?php
+    // contoh penggunaan echo
+    echo "<h1>Papua Tech Community</h1>";
+    echo "Berbagi dan Belajar Bersama Komunitas";
+
+    // contoh penggunaan print
+    print "<h1>Papua Coding School</h1>";
+    print "Belajar Coding dari dasar dengan mengikuti kursus";
+?>
