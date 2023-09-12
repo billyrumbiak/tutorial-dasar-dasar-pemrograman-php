67ac7604950521466997cd5fc677ef5a264ea5e6
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-09-12 16:34:50 +0900

    📝 Add 2 file

---
+++ comment.php
@@ -0,0 +1,11 @@
+<?php
+    // contoh komentar single-line di php
+
+    # ini juga komentar single-line di php
+
+    /*
+        Ini merupakan komentar multipe-lines
+    */
+
+    echo 'PHP Comments';
+?>
---
+++ syntax.php
@@ -0,0 +1,3 @@
+<?php 
+    echo "Hello World"; // code php di ketik disini
+?>
